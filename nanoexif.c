5bdaa61f5d16b3b51aaea0f1c6ec69d6bf84414c
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-03-08 14:40:50 +0900

    renamed

--- nanoexif.c
+++ nanoexif.c
@@ -27,7 +27,7 @@ THE SOFTWARE.
 LIMITATION
 ----------
 
-This module supports C99.
+This module supports C99. I don't want to support legacy systems, but I can accept your patch.
 
 */
 
@@ -53,19 +53,21 @@ http://www.ryouto.jp/f6exif/exif.html
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define HEADER_SIZE (2+2+2+6+2+2+4)
-#define SOI_APP1    "\xFF\xD8\xFF\xE1"
-#define EXIF_HEADER "\x45\x78\x69\x66\x00\x00"
+/* private constants */
+#define NANONANOEXIF_EXIF_HEADER_SIZE (2+2+2+6+2+2+4)
+#define NANOEXIF_SOI_APP1    "\xFF\xD8\xFF\xE1"
+#define NANOEXIF_EXIF_HEADER "\x45\x78\x69\x66\x00\x00"
 
-#define EXIF_THUMBNAIL_TAG_COMPRESSION        0x103
-#define EXIF_THUMBNAIL_TAG_JPEG_IF_OFFSET     0x201
-#define EXIF_THUMBNAIL_TAG_JPEG_IF_BYTE_COUNT 0x202
+#define NANOEXIF_TAG_COMPRESSION        0x103
+#define NANOEXIF_TAG_ORIENTATION        0x112
+#define NANOEXIF_TAG_JPEG_IF_OFFSET     0x201
+#define NANOEXIF_TAG_JPEG_IF_BYTE_COUNT 0x202
 
-#define EXIF_THUMBNAIL_TYPE_SHORT 0x0003
+#define NANOEXIF_TYPE_SHORT 0x0003
 
 typedef enum {
-    EXIF_THUMBNAIL_LITTLE_ENDIAN,
-    EXIF_THUMBNAIL_BIG_ENDIAN,
+    NANOEXIF_LITTLE_ENDIAN,
+    NANOEXIF_BIG_ENDIAN,
 } exif_thumbnail_endian;
 
 typedef struct {
@@ -78,12 +80,11 @@ typedef struct {
 #ifdef DEBUG
 #define D(...) printf(__VA_ARGS__);
 #else
-#define D(x)
+#define D(...)
 #endif
 
-
 static inline uint32_t _read_32(exif_thumbnail_endian endian, const uint8_t *buf) {
-    if (endian == EXIF_THUMBNAIL_LITTLE_ENDIAN) {
+    if (endian == NANOEXIF_LITTLE_ENDIAN) {
         D("LITTLE_ENDIAN\n");
         return (buf[3]<<24) | (buf[2]<<16) | (buf[1]<<8) | buf[0];
     } else {
@@ -92,7 +93,7 @@ static inline uint32_t _read_32(exif_thumbnail_endian endian, const uint8_t *buf
 }
 
 static inline uint16_t _read_16(exif_thumbnail_endian endian, const uint8_t *buf) {
-    if (endian == EXIF_THUMBNAIL_LITTLE_ENDIAN) {
+    if (endian == NANOEXIF_LITTLE_ENDIAN) {
         return (buf[1]<<8) | buf[0];
     } else {
         return (buf[0]<<8) | buf[1];
@@ -106,57 +107,56 @@ static inline uint32_t swap_endian_32(uint32_t i) {
     return ((i&0x000000ff)<<24) | ((i&0x0000ff00)<<8) | ((i&0x00ff0000)>>8) | ((i&0xff000000)>>24);
 }
 
-uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
-    uint8_t buf[HEADER_SIZE];
+uint8_t * nanoexif_thumbnail_read(FILE *fp, size_t *bytes) {
     exif_thumbnail_endian endian;
     exif_thumbnail_endian machine_endian;
+    uint16_t orientation = 0;
 
-    int x=1; // 0x00000001
-    if (*(char*)&x) {
-        /* little endian. memory image 01 00 00 00 */
-        machine_endian = EXIF_THUMBNAIL_LITTLE_ENDIAN;
-    }else{
-        /* big endian. memory image 00 00 00 01 */
-        machine_endian = EXIF_THUMBNAIL_BIG_ENDIAN;
+    {
+        int x=1;
+        machine_endian = (*(char*)&x) ? NANOEXIF_LITTLE_ENDIAN : NANOEXIF_BIG_ENDIAN;
     }
 
-
-    if (fread(buf, sizeof(char), HEADER_SIZE, fp) != HEADER_SIZE) {
-        D("CANNOT OPEN\n");
-        return NULL;
-    }
-    if (memcmp(buf, SOI_APP1, sizeof(SOI_APP1)-1)!=0) {
-        D("SOI\n");
-        return NULL;
-    }
-    if (memcmp(buf+6, EXIF_HEADER, sizeof(EXIF_HEADER)-1)!=0) {
-        D("EXIFHEADER\n");
-        return NULL;
-    }
-    D("ENDIAN: %X\n", buf[12]);
-    if (memcmp(buf+12, "\x4d\x4d", 2) == 0) {
-        D("BIG ENDIAN\n");
-        endian = EXIF_THUMBNAIL_BIG_ENDIAN;
-    } else { // 4949
-        D("LITTLE ENDIAN\n");
-        endian = EXIF_THUMBNAIL_LITTLE_ENDIAN;
-    }
-    if (memcmp(buf+14, "\x00\x2a", 2) != 0) {
-        D("tiff header\n");
-        return NULL; // tiff
-    }
-    uint32_t len = _read_32(endian, buf+16);
-    uint8_t *p = buf+16;
-    D("%d %d %d %d\n", p[0], p[1], p[2], p[3]);
-    if (len != 8) { // normally 8.
-        D("seek : %d\n", len);
-        if (fseek(fp, len-8, SEEK_CUR) != 0) {
-            D("CANNOT SEEK\n");
-            return NULL; // error
+    {
+        uint8_t buf[NANONANOEXIF_EXIF_HEADER_SIZE];
+        if (fread(buf, sizeof(char), NANONANOEXIF_EXIF_HEADER_SIZE, fp) != NANONANOEXIF_EXIF_HEADER_SIZE) {
+            D("CANNOT OPEN\n");
+            return NULL;
+        }
+        if (memcmp(buf, NANOEXIF_SOI_APP1, sizeof(NANOEXIF_SOI_APP1)-1)!=0) {
+            D("SOI\n");
+            return NULL;
+        }
+        if (memcmp(buf+6, NANOEXIF_EXIF_HEADER, sizeof(NANOEXIF_EXIF_HEADER)-1)!=0) {
+            D("EXIFHEADER\n");
+            return NULL;
+        }
+        D("ENDIAN: %X\n", buf[12]);
+        if (memcmp(buf+12, "\x4d\x4d", 2) == 0) {
+            D("BIG ENDIAN\n");
+            endian = NANOEXIF_BIG_ENDIAN;
+        } else { // 4949
+            D("LITTLE ENDIAN\n");
+            endian = NANOEXIF_LITTLE_ENDIAN;
+        }
+        if (memcmp(buf+14, "\x00\x2a", 2) != 0) {
+            D("tiff header\n");
+            return NULL; // tiff
+        }
+        uint32_t len = _read_32(endian, buf+16);
+        uint8_t *p = buf+16;
+        D("%d %d %d %d\n", p[0], p[1], p[2], p[3]);
+        if (len != 8) { // normally 8.
+            D("seek : %d\n", len);
+            if (fseek(fp, len-8, SEEK_CUR) != 0) {
+                D("CANNOT SEEK\n");
+                return NULL; // error
+            }
         }
     }
+
     {
-        // skip ifd0
+        // ifd0
         {
             uint8_t ifdcntbuf[2];
             int read = fread(ifdcntbuf, sizeof(char), sizeof(ifdcntbuf), fp);
@@ -164,12 +164,39 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
                 D("cannot read 1: %d\n", read);
                 return NULL;
             }
-            uint16_t cnt = _read_16(endian, ifdcntbuf);
-#define IFD_FIELD_SIZE 12
-            if (fseek(fp, cnt*IFD_FIELD_SIZE, SEEK_CUR) != 0) {
-                D("cannot seek 2\n");
-                return NULL;
+            uint16_t entry_num = _read_16(endian, ifdcntbuf);
+            exif_thumbnail_ifd_entry entry;
+            int i;
+            for (i=0; i<entry_num; i++) {
+                D("-- %d\n", i);
+                if (fread(&entry, sizeof(exif_thumbnail_ifd_entry), 1, fp) != 1) {
+                    D("cannot read\n");
+                    return NULL;
+                }
+
+                if (machine_endian != endian) {
+                    entry.tag    = swap_endian_16(entry.tag);
+                    entry.type   = swap_endian_16(entry.type);
+                    entry.count  = swap_endian_32(entry.count);
+                }
+                switch (entry.tag) {
+                case NANOEXIF_TAG_ORIENTATION:
+                    if (entry.type != NANOEXIF_TYPE_SHORT) {
+                        D("oops\n");
+                        return 0;
+                    }
+                    orientation = _read_16(endian, entry.offset);
+                    D("orientation: %d\n", orientation);
+                    break;
+                }
+                // TODO: endian
+                D("tag: %X\ntype: %X\ncount: %d\n",
+                    entry.tag,
+                    entry.type,
+                    entry.count
+                );
             }
+#define IFD_FIELD_SIZE 12
         }
         {
             uint8_t lenbuf[4];
@@ -179,12 +206,13 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
             }
             uint32_t skip = _read_32(endian, lenbuf);
             D("SEEK TO skip; %d\n", skip);
-            if (fseek(fp, HEADER_SIZE+skip-8, SEEK_SET) != 0) {
+            if (fseek(fp, NANONANOEXIF_EXIF_HEADER_SIZE+skip-8, SEEK_SET) != 0) {
                 D("cannot seek 4\n");
                 return NULL;
             }
         }
     }
+
     {
         // process ifd1
         uint8_t ifdcntbuf[2];
@@ -201,12 +229,7 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
         uint32_t jpeg_byte_count = 0;
         for (i=0; i<entry_num; i++) {
             D("-- %d\n", i);
-            if (fread(&entry, sizeof(exif_thumbnail_ifd_entry), 1, fp) != 2) {
-                D("tag: %X\ntype: %X\ncount: %d\n",
-                    entry.tag,
-                    entry.type,
-                    entry.count
-                );
+            if (fread(&entry, sizeof(exif_thumbnail_ifd_entry), 1, fp) != 2) { // TODO: fix 2
                 if (machine_endian != endian) {
                     entry.tag    = swap_endian_16(entry.tag);
                     entry.type   = swap_endian_16(entry.type);
@@ -219,9 +242,9 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
                     entry.count
                 );
                 switch (entry.tag) {
-                case EXIF_THUMBNAIL_TAG_COMPRESSION:
+                case NANOEXIF_TAG_COMPRESSION:
                     {
-                        if (entry.type != EXIF_THUMBNAIL_TYPE_SHORT) {
+                        if (entry.type != NANOEXIF_TYPE_SHORT) {
                             D("unknown type: %d\n", entry.type);
                             return NULL;
                         }
@@ -235,14 +258,14 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
                         }
                         break;
                     }
-                case EXIF_THUMBNAIL_TAG_JPEG_IF_OFFSET:
+                case NANOEXIF_TAG_JPEG_IF_OFFSET:
                     {
                         uint32_t offset = _read_32(endian, entry.offset);
                         D("offset: %d\n", offset);
                         jpeg_offset = offset;
                         break;
                     }
-                case EXIF_THUMBNAIL_TAG_JPEG_IF_BYTE_COUNT:
+                case NANOEXIF_TAG_JPEG_IF_BYTE_COUNT:
                     {
                         uint32_t offset = _read_32(endian, entry.offset);
                         D("byte_count: %d\n", offset);
@@ -258,7 +281,7 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
             return NULL;
         }
         D("jpeg offset: %X\n", jpeg_offset);
-        if (fseek(fp, HEADER_SIZE-8+jpeg_offset, SEEK_SET) != 0) {
+        if (fseek(fp, NANONANOEXIF_EXIF_HEADER_SIZE-8+jpeg_offset, SEEK_SET) != 0) {
             D("cannot seek 9\n");
             return NULL;
         }
@@ -280,34 +303,29 @@ uint8_t * exif_thumbnail_read(FILE *fp, size_t *bytes) {
 }
 
 #ifdef TEST
+
+#include "nanotap.h"
+
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("Usage: %s src.jpg thumb.jpg\n", argv[0]);
-        return 2;
-    }
+    const char * src = "t/data/sample-iphone.jpg";
     FILE *ifp;
-    if (!(ifp = fopen(argv[1], "rb"))) {
+    if (!(ifp = fopen(src, "rb"))) {
         perror(argv[0]);
         return 1;
     }
     size_t bytes = 0;
-    uint8_t* thumb = exif_thumbnail_read(ifp, &bytes);
-    if (!thumb) {
+    uint8_t* thumb = nanoexif_thumbnail_read(ifp, &bytes);
+    ok(!!thumb, "thumbnail generated");
+    if (!(thumb && bytes > 100)) {
         printf("cannot load thumbnail\n");
         return 0;
     }
-    FILE *ofp;
-    if (!(ofp = fopen(argv[2], "wb"))) {
-        perror(argv[0]);
-        return 1;
-    }
-    if (fwrite(thumb, sizeof(uint8_t), bytes, ofp) != bytes) {
-        perror(argv[0]);
-        return 1;
-    }
+    ok(memcmp(thumb, "\xFF\xD8", 2)==0, "jpeg?");
+    ok(memcmp(thumb+(bytes-2), "\xFF\xD9", 2)==0, "jpeg eoi");
     free(thumb);
     fclose(ifp);
-    fclose(ofp);
+
+    done_testing();
     return 0;
 }
 #endif
